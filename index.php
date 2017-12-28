113ca4b14f755d768529fa2b1dd34929a3eb9d57
William Correa <wilcorrea@gmail.com>
2017-12-27 22:49:59 -0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
-<style>
-html {
-   width: 100vw; 
-   height: 100vh; 
-   background:url('lyseon-tech.jpg') center center no-repeat;
-}
-</style>
+<html>
+   <head>
+      <title>Lyseon Tech</title>
+   </head>
+   <body>
+      <style>
+         html {
+            width: 100vw; 
+            height: 100vh; 
+            background: url('lyseon-tech.jpg') center center no-repeat;
+            background-size: 200px;
+         }
+      </style>
+   </body>
+</html>
