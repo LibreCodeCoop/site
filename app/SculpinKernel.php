51e8538d4df1c27456b968ec501616f0b86f9125
Vitor Mattos <vitor.mattos@phprio.org>
2018-01-08 15:39:23 -0200

    Sculpin Kernel

---
+++ app/SculpinKernel.php
@@ -0,0 +1,11 @@
+<?php
+
+use Sculpin\Bundle\SculpinBundle\HttpKernel\AbstractKernel;
+
+class SculpinKernel extends AbstractKernel
+{
+    protected function getAdditionalSculpinBundles()
+    {
+        return [];
+    }
+}
