54ffe82bd7eea1b11d1eef9f3f67180cb593b61c
Lorhana Trindade <lortrindade@gmail.com>
2018-02-13 09:25:53 -0200

    Refazendo estrutura da pagina de solucoes

---
+++ source/solucoes.blade.php
@@ -0,0 +1,47 @@
+<?php
+$page->title = 'Soluções';
+$page->subtitle = 'Conheça nossas soluções';
+?>
+@extends('_layouts.page')
+
+@section('body')
+<!-- Section -->
+<section class="md-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-10 col-xl-8 offset-0 offset-sm-0 offset-md-0 offset-lg-1 offset-xl-2 ">
+				
+				<!-- title-01 -->
+				<div class="title-01 title-01__style-04">
+					<h6 class="title-01__subTitle">{{ $page->title }}</h6>
+					<h2 class="title-01__title">Lyseon Tech - Soluções Tecnológicas</h2>
+					<div>A Lyseon Tech é uma cooperativa de trabalho com modelo de gestão democrática, segura e eficiente composta por profissionais de T.I. altamente qualificados e experientes no mercado. Prezamos por apresentar vantagens, tanto para o cooperado como para as empresas parceiras e para isso oferecemos para seu negócio as seguintes soluções:
+					<ul class="sobre-solucoes">
+					   <li>Desenvolvimento de Sistemas (web, mobile e desktop)</li>
+                     <li>Consultoria Geral em T.I.</li>
+                     <li>Projeto e administração de Banco de Dados</li>
+                     <li>Consultoria em Informações Gerenciais</li>
+                     <li>Análise de Softwares</li>
+                     <li>Auditoria de Sistemas</li>
+                     <li>Treinamentos Especializados</li>
+                     <li>Outsourcing</li>
+                     <li>Projetos de Redes Locais</li>
+                     <li>Marketing Digital</li>
+                     <li>Criação de Logomarca</li>
+                     <li>Criação e reestruração de identidade visual</li>
+					</ul>
+					</div>
+				</div><!-- End / title-01 -->
+				
+			</div>
+		</div>
+	</div>
+</section>
+<!-- End / Section -->
+
+
+
+
+
+<!-- End / Section -->
+@endsection
