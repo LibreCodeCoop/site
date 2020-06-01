389520dc611a73cd26f0462d513d7c8576c9245a
username <username@europe>
2020-06-01 13:54:07 -0300

    bootstrap com jigsaw

---
+++ webpack.mix.js
@@ -0,0 +1,13 @@
+const mix = require('laravel-mix');
+require('laravel-mix-jigsaw');
+
+mix.disableSuccessNotifications();
+mix.setPublicPath('source/assets/build');
+
+mix.jigsaw()
+    .js('source/_assets/js/main.js', 'js')
+    .sass('source/_assets/sass/main.scss', 'css')
+    .options({
+        processCssUrls: false,
+    })
+    .version();
