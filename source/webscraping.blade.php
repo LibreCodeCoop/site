6975f8ec3baa79380a2d2c81898c0515fb014c36
Daiane Alves <daiane@lt.coop.br>
2020-06-02 20:49:52 -0300

    Adição da página webscraping

---
+++ source/webscraping.blade.php
@@ -0,0 +1,65 @@
+@extends('_layouts.master')
+
+@section('body')
+
+<section id="intro" class="clearfix">
+    <div class="container">
+      <div class="intro-info">
+          <h2>Web scrapping</h2>   
+      </div>
+    </div>    
+  </section><!-- #intro -->
+
+  <main id="main">
+
+    <!--==========================
+      About Us Section
+    ============================-->
+    <section id="about" class="clearfix">
+      <div class="container">
+
+        <div class="row about-container">
+
+          <div class="col-lg-12 content order-lg-1 order-2">
+
+            <div class="icon-box wow fadeInUp">
+              <p class="description">
+              Faça coleta de dados da internet através de processos automatizados, implementados usando webscraping. Nossa ferramenta facilita a busca, manipulação e análise de dados que podem ser usados como estratégia para a otimização de vendas e personalização do atendimento a clientes.<br>
+              Você pode utilizar webscraping para fazer comparação de preços online, acompanhar mudança de sites, integrações de informações para relatórios financeiros, dados meteorológicos, pesquisa e comportamento na web, etc. Veja como pode utilizar webscraping na sua empresa:</p>
+            </div>              
+            <div class="icon-box wow fadeInUp" data-wow-delay="0.2s">
+              <h4 class="title">
+                Recursos humanos
+              </h4>
+              <p class="description">
+                Podem usar essa ferramenta para obter informações durante o processo de recrutamento e até mesmo utilizar a técnica para coletar e filtrar os dados de diferentes sites de empregos de uma só vez.</p>
+            </div>
+            <div class="icon-box wow fadeInUp" data-wow-delay="0.2s">
+              <h4 class="title">
+              Varejo ou Indústria
+            </h4>
+              <p class="description">
+                É possível capturar dados sobre as descrições de produtos e possíveis problemas que seus consumidores enfrentam sobre os seus produtos apenas usando as informações e opiniões do usuário final disponível em redes sociais, sites de avaliação, páginas e fóruns.
+              </p>
+            </div>
+            <div class="icon-box wow fadeInUp" data-wow-delay="0.2s">
+              <h4 class="title">
+                Mineração de dados
+              </h4>
+              <p class="description">
+                 O uso de web scraping pode ser utilizado para estudos na área de mineração de textos e artigos.
+              </p>
+            </div>
+            <div class="icon-box wow fadeInUp" data-wow-delay="0.2s">
+              <h4 class="title">
+                Webcraping nos negócios
+              </h4>
+              <p class="description">
+                Faça monitoramento de concorrentes, extração de preços e com base nestes dados você poderá realizar uma análise de mercado mais eficiente ajudando na definição de estratégias para expansão dos negócios.
+              </p>
+            </div>
+        </div>
+      </div>
+    </section><!-- #about -->
+  </main>
+@endsection
