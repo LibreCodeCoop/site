bf010be5a484283dc6b9de70791eba52d52bae39
Vinicios Gomes <viniciusgomesviana@gmail.com>
2021-11-08 13:12:11 -0300

    add basic structure

---
+++ src/components/SearchBar/index.tsx
@@ -0,0 +1,11 @@
+import style from "./styles.module.scss";
+
+export const SearchBar = () => {
+  return (
+    <>
+      <div className={style.container}>
+        <h1>Search Bar</h1>
+      </div>
+    </>
+  );
+};
