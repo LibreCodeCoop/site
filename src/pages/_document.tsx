a610ef2d8ba9d1bf3b6a963661c9c14e0f5617ed
Vinicios Gomes <viniciusgomesviana@gmail.com>
2021-11-11 11:16:44 -0300

    add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,7 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
             rel="stylesheet"
           />
+          <link rel="shortcut icon" href="/static/favicon.png" />
         </Head>
         <body>
           <Main />
