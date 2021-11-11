04648f8fe015288ef8e4583257d7effdbe10bff6
Vinicios Gomes <viniciusgomesviana@gmail.com>
2021-11-11 13:44:25 -0300

    add ActiveLink

---
+++ src/components/ActiveLink.tsx
@@ -0,0 +1,36 @@
+import { useRouter } from "next/router";
+import Link, { LinkProps } from "next/link";
+import { cloneElement, ReactElement } from "react";
+
+interface ActiveLinkProps extends LinkProps {
+  children: ReactElement;
+  shouldMatchExactHref?: boolean;
+}
+
+export function ActiveLink({
+  children,
+  shouldMatchExactHref = false,
+  ...rest
+}: ActiveLinkProps) {
+  const { asPath } = useRouter();
+  let isActive = false;
+
+  if (shouldMatchExactHref && (asPath === rest.href || asPath === rest.as)) {
+    isActive = true;
+  }
+
+  if (
+    !shouldMatchExactHref &&
+    (asPath.startsWith(String(rest.href)) || asPath.startsWith(String(rest.as)))
+  ) {
+    isActive = true;
+  }
+
+  return (
+    <Link {...rest}>
+      {cloneElement(children, {
+        color: isActive ? "gray.600" : "gray.50",
+      })}
+    </Link>
+  );
+}
