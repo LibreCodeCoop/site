da86d8e7f6a41e982e46c0330ac8bd909313139c
Vinicios Gomes <viniciusgomesviana@gmail.com>
2021-11-17 17:53:52 -0300

    add weight to font roboto

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300;400;500;700;800;900&display=swap"
             rel="stylesheet"
           />
           <link rel="shortcut icon" href="/static/favicon.png" />
