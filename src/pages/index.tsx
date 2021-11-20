4c395cf98ec9b8f201346699d72f44a095e22c98
Vinicios Gomes <viniciusgomesviana@gmail.com>
2021-11-20 17:20:47 -0300

    fix responsive style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,6 @@ interface AnchorProps {
   type: "left" | "right" | "hexagonal";
   url: string;
 }
-
 interface ContentSections {
   title: DescriptionProps;
   image: string;
@@ -21,15 +20,29 @@ interface ContentSections {
   anchor: AnchorProps;
   bgColor: string;
 }
-
 interface ContentProps {
   title: DescriptionProps;
   image: string;
   sections: ContentSections[];
 }
-
 interface HomeProps {
   data: ContentProps;
+}
+interface AlignReturn {
+  align: string[] | string;
+  ml?: string[] | string;
+  alignDescription?: string[] | string;
+}
+
+enum SectionMarginTop {
+  guiamos = "-70px",
+  criamos = "-130px",
+  desenvolvemos = "-170px",
+  implantamos = "-110px",
+  aprimoramos = "-170px",
+  integramos = "-80px",
+  treinamos = "-15px",
+  mantemos = "-165px",
 }
 
 export default function Home({ data }: HomeProps) {
@@ -47,23 +60,48 @@ export default function Home({ data }: HomeProps) {
   function getMarginTop(section: ContentSections) {
     switch (section.title.name.toLowerCase()) {
       case "guiamos":
-        return "-80px";
+        return SectionMarginTop.guiamos;
       case "criamos":
-        return "-130px";
+        return SectionMarginTop.criamos;
       case "desenvolvemos":
-        return "-165px";
+        return SectionMarginTop.desenvolvemos;
       case "implantamos":
-        return "-110px";
+        return SectionMarginTop.implantamos;
       case "aprimoramos":
-        return "-165px";
+        return SectionMarginTop.aprimoramos;
       case "integramos":
-        return "-80px";
+        return SectionMarginTop.integramos;
       case "treinamos":
-        return "-15px";
+        return SectionMarginTop.treinamos;
       case "mantemos":
-        return "-155px";
+        return SectionMarginTop.mantemos;
       default:
-        return "0";
+        return 0;
+    }
+  }
+
+  function getAlign(section: ContentSections): AlignReturn {
+    switch (section.title.name.toLowerCase()) {
+      case "desenvolvemos":
+        return {
+          align: ["center", "center", "center", "flex-start"],
+          ml: ["2", "2", "20"],
+        };
+      case "implantamos":
+        return { align: "center", ml: ["2", "2", "2", "64"] };
+      case "aprimoramos":
+        return {
+          align: ["center", "center", "center", "flex-end"],
+          alignDescription: ["inherit", "inherit", "center"],
+        };
+      case "integramos":
+        return { align: ["center", "center", "center", "flex-end"] };
+      case "treinamos":
+        return { align: ["center", "center", "center", "flex-start"] };
+      case "mantemos":
+        return { align: ["center", "center", "center", "flex-end"] };
+      default:
+        return { align: "center", ml: [] };
     }
   }
 
@@ -80,13 +118,17 @@ export default function Home({ data }: HomeProps) {
         align="center"
         direction="column"
       >
-        <Text fontSize="6xl" color={data.title.font_color} fontWeight="bold">
+        <Text
+          fontSize={["5xl", "8xl"]}
+          color={data.title.font_color}
+          fontWeight="bold"
+        >
           {data.title.name}
         </Text>
         <Divider w="40px" />
-
-        <Icon as={MouseScroll} fontSize="3xl" />
+        <Icon mt="10" as={MouseScroll} fontSize="5xl" />
       </Flex>
+
       {data.sections.map((section, index) => (
         <Flex
           as="section"
@@ -94,29 +136,54 @@ export default function Home({ data }: HomeProps) {
           key={`${section.title.name}-${index}`}
           bgImage={section.image}
           bgSize="cover"
-          h="920px"
+          h="950px"
           bgRepeat="no-repeat"
           bgPos="top center"
           zIndex={index}
           justify="center"
-          align="center"
+          align={getAlign(section).align}
           direction="column"
           mt={getMarginTop(section)}
+          pl={getAlign(section).ml}
+          px={{ lg: "4" }}
         >
           <Text
-            fontSize="6xl"
+            fontSize={["2xl", "5xl", "5xl", "9xl"]}
             color={section.title.font_color}
             fontWeight="bold"
+            alignSelf={getAlign(section).alignDescription}
           >
             {section.title.name}
           </Text>
-          <Text>{section.description.name}</Text>
-          <ButtonLink
-            type={ReturnTypeButton(section.anchor.type)}
-            url={section.anchor.url}
-            text={section.anchor.name}
-            font_size={section.title.name === "Guiamos" ? 34 : 23}
-          />
+
+          <Flex
+            direction="column"
+            justify="center"
+            align="center"
+            w={["full", "full", "full", "md"]}
+          >
+            <Text
+              fontSize={["xl", "xl", "4xl"]}
+              fontWeight="medium"
+              color={section.description.font_color}
+              mb="10"
+              mx={2}
+              textAlign="center"
+            >
+              {section.description.name}
+            </Text>
+
+            <ButtonLink
+              type={ReturnTypeButton(section.anchor.type)}
+              url={section.anchor.url}
+              text={section.anchor.name}
+              font_size={
+                section.anchor.name.toLowerCase().includes("peça seu ")
+                  ? 34
+                  : 23
+              }
+            />
+          </Flex>
         </Flex>
       ))}
     </Flex>
