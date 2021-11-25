0968472e1c03d13cb9453264c2b4d816f9f993e0
Vinicios Gomes <viniciusgomesviana@gmail.com>
2021-11-25 20:22:45 -0300

    add get Content and typing

--- src/pages/about-us/software-livre.tsx
+++ src/pages/about-us/software-livre.tsx
@@ -1,4 +1,31 @@
 import { Text } from "@chakra-ui/react";
-export default function SoftwareLivre() {
+import Content from "@/content/about-us/software-livre.json";
+
+interface SoftwareLivreProps {
+  data: IContent;
+}
+
+interface IContent {
+  title: string;
+  description: string;
+  pilars: string[];
+  subSection: ISubSection;
+}
+
+interface ISubSection {
+  title: string;
+  description: string;
+  anchor: string;
+}
+
+export default function SoftwareLivre({ data }: SoftwareLivreProps) {
   return <Text>Page in development...</Text>;
+}
+
+export function getStaticProps() {
+  return {
+    props: {
+      data: Content,
+    },
+  };
 }
