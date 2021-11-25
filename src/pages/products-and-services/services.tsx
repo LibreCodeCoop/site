c763a71cbd94ffc41a2e42af9a73c7f8615e3c5e
Vinicios Gomes <viniciusgomesviana@gmail.com>
2021-11-25 11:36:45 -0300

    add interfaces and load Content

--- src/pages/products-and-services/services.tsx
+++ src/pages/products-and-services/services.tsx
@@ -1,4 +1,36 @@
 import { Text } from "@chakra-ui/react";
-export default function Services() {
+import Content from "@/content/products-and-services/services.json";
+
+interface ServicesProps {
+  data: IContent;
+}
+interface IContent {
+  title: string;
+  description: string;
+  services: IServices[];
+}
+
+interface IServices {
+  title: string;
+  description: string;
+  features: string[];
+  anchor: IAnchor;
+}
+
+interface IAnchor {
+  type: string;
+  name: string;
+  url: string;
+}
+
+export default function Services({ data }: ServicesProps) {
   return <Text>Page in development...</Text>;
+}
+
+export function getStaticProps() {
+  return {
+    props: {
+      data: Content,
+    },
+  };
 }
