2b0364f22e04432b8934f67a877b71c143a38e30
Vinicios Gomes <viniciusgomesviana@gmail.com>
2021-11-26 10:28:54 -0300

    add content and typing

--- src/pages/about-us/members.tsx
+++ src/pages/about-us/members.tsx
@@ -1,4 +1,35 @@
 import { Text } from "@chakra-ui/react";
-export default function Members() {
+import Content from "@/content/about-us/nossos-cooperados.json";
+
+interface MembersProps {
+  data: IContent;
+}
+interface IContent {
+  title: string;
+  image: string;
+  background: string;
+  description: string;
+  cooperados: ICooperados[];
+}
+interface ICooperados {
+  name: string;
+  avatar: IAvatar;
+  position: string;
+  email: string;
+}
+interface IAvatar {
+  useGravatarApi: boolean;
+  localPath?: string;
+}
+
+export default function Members({ data }: MembersProps) {
   return <Text>Page in development...</Text>;
+}
+
+export function getStaticProps() {
+  return {
+    props: {
+      data: Content,
+    },
+  };
 }
