8df79950263f4111b188312a2f27d8213ace68f5
Vinicios Gomes <viniciusgomesviana@gmail.com>
2021-11-28 10:12:17 -0300

    style

--- src/components/MemberCard/index.tsx
+++ src/components/MemberCard/index.tsx
@@ -21,17 +21,26 @@ export function MemberCard({
 }: MemberCardProps) {
   function gravatarUrl(email: string): string {
     const encodedEmail = MD5(email.trim()).toString();
-    return `https://gravatar.com/avatar/${encodedEmail}`;
+    return `https://gravatar.com/avatar/${encodedEmail}?s=400`;
   }
   return (
-    <Flex direction="column">
+    <Flex
+      color="purple.1000"
+      mx="16"
+      my="12"
+      justify="center"
+      align="center"
+      direction="column"
+    >
       <Avatar
         name={name}
+        size={"2xl"}
         src={avatar.useGravatarApi ? gravatarUrl(email) : avatar.localPath}
+        mb="4"
       />
-      <Text>{name}</Text>
-      <Text>{jobPosition}</Text>
-      <Text>{email}</Text>
+      <Text fontWeight="bold">{name}</Text>
+      <Text fontWeight="bold">{jobPosition}</Text>
+      <Text fontWeight="bold">{email}</Text>
     </Flex>
   );
 }
