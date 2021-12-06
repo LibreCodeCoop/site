9ca34c8c77e8a26d70301617150a9920d1d79ae7
Vinicios Gomes <viniciusgomesviana@gmail.com>
2021-12-06 14:02:10 -0300

    ComponentLogo

---
+++ src/components/OportunitiesCard/LogoLibreCode.tsx
@@ -0,0 +1,9 @@
+import { Flex } from "@chakra-ui/react";
+import Logo from "../Header/Logo";
+export function LogoLibreCode() {
+  return (
+    <Flex justify="center" align="center" bg="gray.50" w="130px" h="130px">
+      <Logo />
+    </Flex>
+  );
+}
