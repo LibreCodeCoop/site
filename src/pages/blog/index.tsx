6b19a6e7b50d15ec6e2fabb9c8a989ccaeed1561
Vinicios Gomes <viniciusgomesviana@gmail.com>
2021-12-08 10:50:12 -0300

    add header

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,6 +1,7 @@
-import { Text } from "@chakra-ui/react";
+import { Flex, Text, Icon } from "@chakra-ui/react";
 import { GetStaticProps } from "next";
 import Content from "@/content/blog.json";
+import { MouseScroll } from "@/components/MouseScroolIcon";
 
 interface BlogProps {
   data: IContent;
@@ -16,7 +17,33 @@ interface IHeader {
 }
 
 export default function Blog({ data }: BlogProps) {
-  return <Text>Page in development...</Text>;
+  return (
+    <Flex direction="column">
+      <Flex
+        bgImage={data.header.background}
+        bgPos="bottom"
+        bgRepeat="no-repeat"
+        bgSize="cover"
+        h="500px"
+        justify="center"
+        align="center"
+        direction="column"
+      >
+        <Text
+          fontSize={{ base: "6xl", xl: "8xl" }}
+          fontWeight="bold"
+          color="gray.50"
+        >
+          {data.header.title}
+        </Text>
+        <Icon
+          mt={{ base: "10", lg: "14" }}
+          as={MouseScroll}
+          fontSize={["4xl", "5xl"]}
+        />
+      </Flex>
+    </Flex>
+  );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
