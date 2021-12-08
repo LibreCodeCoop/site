48182629b990c1da0f91fdfc866c11909b67352e
Vinicios Gomes <viniciusgomesviana@gmail.com>
2021-12-08 10:43:20 -0300

    add interfaces and get content and pass to page

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,4 +1,28 @@
 import { Text } from "@chakra-ui/react";
-export default function Blog() {
+import { GetStaticProps } from "next";
+import Content from "@/content/blog.json";
+
+interface BlogProps {
+  data: IContent;
+}
+
+interface IContent {
+  header: IHeader;
+}
+
+interface IHeader {
+  title: string;
+  background: string;
+}
+
+export default function Blog({ data }: BlogProps) {
   return <Text>Page in development...</Text>;
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {
+      data: Content,
+    },
+  };
+};
