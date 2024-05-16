03417b827df03d7abd8f0055551ab8076b03dc0c
Crisciany <crisciany.souza@librecode.coop>
2024-05-16 16:13:17 -0400

    Change in the configuration
    Signed-off-by: Crisciany <crisciany.souza@librecode.coop>

---
+++ .docker/php/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Set uid of host machine
+usermod --non-unique --uid "${HOST_UID}" www-data
+groupmod --non-unique --gid "${HOST_GID}" www-data
+
+if [ ! -d "vendor" ]; then
+    composer i
+fi
+php-fpm &
+if [[ "$SERVER_MODE" == 'watch' ]]; then
+    npm run watch
+else
+    php ./vendor/bin/jigsaw serve --host 0.0.0.0 --port 3000
+fi
