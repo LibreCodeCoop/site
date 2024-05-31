8d7754878a977b2d1c178cec786a6594ea5c6dfa
Crisciany Souza <crisciany.souza@librecode.coop>
2024-05-31 17:17:13 -0400

    feat:add () in funtion getUrl()
    Signed-off-by: Crisciany Souza <crisciany.souza@librecode.coop>

--- source/posts.blade.php
+++ source/posts.blade.php
@@ -2,7 +2,7 @@
 @section('body')
     <section id="intro-blog">
         <div class="container">
-            <h1 class="text-center mt-5">Blog</h1>
+            <h1 class="text-center font-weight-bold mt-5">Blog</h1>
             <p class="description mt-5 mb-5 text-justify">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius molestie pharetra. Curabitur et est sagittis, elementum magna et, sodales ante. Cras sit amet consequat dolor. Nulla ac nisi metus. Suspendisse fermentum purus vitae ante imperdiet ornare. Duis aliquam rutrum semper. Ut egestas a ligula mollis dictum.
             </p>
@@ -20,13 +20,13 @@
                         <div class="col-lg-3 mb-3">
                             <article class="article-blog">
                                 <div class="post-img">
-                                    <a href="{{ $post->getUrl }}">
+                                    <a href="{{ $post->getUrl() }}">
                                         <img src="{{ $post->cover_image }}" class="img-fluid">
                                     </a>
                                 </div>
                                 <p class="post-category">{{ date('F j, Y', $post->date) }}</p>
                                 <h2 class="title">
-                                    <a href="{{ $post->getUrl }}">{{ $post->title }}</a>
+                                    <a href="{{ $post->getUrl() }}">{{ $post->title }}</a>
                                 </h2>
                                 <p>{{ $post->description }}</p>
                             </article>
