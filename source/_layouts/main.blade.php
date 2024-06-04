ef491e3fe3fb5ae85e4b491e306a6929b7481869
Crisciany Souza <54338576+Any97Cris@users.noreply.github.com>
2024-06-04 17:02:21 -0400

    Update main.blade.php
    remove lineicons link

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -24,10 +24,6 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css" />
   <script src="https://cdn.jsdelivr.net/gh/mcstudios/glightbox/dist/js/glightbox.min.js"></script>
 
-  <!--Icons-->
-  <link rel="stylesheet" href="https://cdn.lineicons.com/4.0/lineicons.css" />
-
-
 </head>
 
 <body>
