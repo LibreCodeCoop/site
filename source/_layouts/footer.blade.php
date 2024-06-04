b74cfe0934472132ba5e24a80bb17bd7fc69bde8
Crisciany Souza <crisciany.souza@librecode.coop>
2024-06-04 14:31:31 -0400

    remove scroll top
    Signed-off-by: Crisciany Souza <crisciany.souza@librecode.coop>

--- source/_layouts/footer.blade.php
+++ source/_layouts/footer.blade.php
@@ -44,8 +44,6 @@
         </div>
     </div>
 
-    <!-- Scroll Top -->
-    <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
 </footer>
 
