c113cea44a287e37781b14298e7523a014016765
Crisciany Souza <crisciany.souza@librecode.coop>
2024-09-10 14:47:11 -0400

    change style of page
    Signed-off-by: Crisciany Souza <crisciany.souza@librecode.coop>

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -17,25 +17,29 @@
                 <div class="row justify-content-md-center">
 
                     <div class="col-lg-8 pt-5">
+                        <h4 class="text-center mb-5">Vamos quebrar o gelo.</h4>
+                        <p class="text-center mb-5 p-3">Estamos aqui para ajudar! Se você tiver alguma dúvida, sugestão ou precisar de mais informações sobre nossos produtos e serviços, não hesite em nos contatar. </p>
                         {{-- <h5 class="text-center">Contate-nos</h5> --}}
                         <iframe id="form-contato" src="https://ls.librecode.coop/?r=survey/index&sid=467949&newtest=Y" style="width: 100%;height:460px;border: 0;overflow:hidden;" scrolling="no"></iframe>
                     </div>
-
+                    
                     <div class="col-lg-4 contact-us-div pt-5">
-                        <h4 class="text-center mb-5">Vamos quebrar o gelo.</h4>
-                        <p class="text-center mb-5 p-3">Estamos aqui para ajudar! Se você tiver alguma dúvida, sugestão ou precisar de mais informações sobre nossos produtos e serviços, não hesite em nos contatar. </p>
-
+                        <h4 class="text-center mb-5">Contate-nos</h4>
+                        
                         <div class="row justify-content-md-center">
-                            <div class="col-lg-3 text-center">
+                            <div class="col-lg-12 col-sm-6 text-center mb-5">
                                 <a href="tel:+55(21)2042-2073"><i class="lni lni-phone contact-icon"></i></a>
+                                <p class="mt-3">+55(21)2042-2073</p>
                             </div>
-                            <div class="col-lg-3 text-center">
+                            <div class="col-lg-12 col-sm-6 text-center">
                                 <a href="mailto:contato@librecode.coop"><i class="lni lni-envelope text-center contact-icon"></i></a>
+                                <p class="mt-3">contato@librecode.coop</p>
                             </div>
                         </div>
-                        <p class="text-center mt-5 p-5">Agradecemos pelo seu contato e estaremos ansiosos para ajudar!</p>
-                    </div>
 
+                    </div>
+                    
+                    <p class="text-center mt-4">Agradecemos pelo seu contato e estaremos ansiosos para ajudar!</p>
                 </div>
             </div>
         </section>
